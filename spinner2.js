5b54fafaa8fe8678dec0560b7bc103623e3051ea
Hamza <Hamzanur2010@hotmail.com>
2021-12-05 00:16:24 +0000

    spinner2

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+const stdOut = process.stdout;
+let counter = 100;
+let i = 0;
+let offset = 200;
+for (const item of array) {
+  setTimeout(() => {
+    stdOut.write("\r" + item);
+  }, counter + i * offset);
+  i++;
+}
